04a17d10b09de70e3d21972f0a28d312c61d9657
Chris Owens <guitarrockstarco@gmail.com>
2020-09-10 19:53:48 -0400

    Setting up read file program

---
+++ read_file.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readlind.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
