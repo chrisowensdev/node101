fc00c310df876c3068bf5003be6b23c4468a9224
Chris Owens <guitarrockstarco@gmail.com>
2020-09-10 20:00:30 -0400

    Read file contents and console log contents uppercased

--- read_file.js
+++ read_file.js
@@ -3,7 +3,17 @@
 const fs = require('fs');
 const readline = require('readline');
 
-const rl = readlind.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+rl.question("filename: ", (filename) => {
+    fs.readFile(filename, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(data.toString().toUpperCase());
+    })
+    rl.close();
+})
